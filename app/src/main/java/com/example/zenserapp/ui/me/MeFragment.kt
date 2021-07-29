8b71c3e8049165ddcec48d18592e559f17cf3cc4
wmchong <87300821+wmchong@users.noreply.github.com>
2021-07-30 00:06:55 +0800

    added viewBinding to build.gradle changed color scheme(for fun) added EditText and ImageButton to MeFragment

--- app/src/main/java/com/example/zenserapp/ui/me/MeFragment.kt
+++ app/src/main/java/com/example/zenserapp/ui/me/MeFragment.kt
@@ -1,18 +1,18 @@
 package com.example.zenserapp.ui.me
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.zenserapp.R
+import com.example.zenserapp.databinding.FragmentMeBinding
+import android.util.Log
 
-class MeFragment : Fragment() {
+class MeFragment : Fragment(R.layout.fragment_me) {
 
-    companion object {
-        fun newInstance() = MeFragment()
-    }
+    private var _binding: FragmentMeBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var viewModel: MeViewModel
 
@@ -20,13 +20,26 @@ class MeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_me, container, false)
+
+        _binding = FragmentMeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchIB.setOnClickListener {
+            //send text to backend
+            //sql here
+            val text = binding.searchET.text.toString()
+            Log.i("look here",text)
+        }
+    }
+
+    //prevent memory leaks
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
